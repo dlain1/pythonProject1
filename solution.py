4af9faacb39a7a6e24e359e620580b48e7d7d781
dlain1 <90530931+dlain1@users.noreply.github.com>
2021-09-17 18:34:05 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -3,29 +3,31 @@
 ### Output - The right answer for the specific question.
 
 def welcome_assignment_questions(question: object) -> object:
-    #The student doesn't have to follow the skeleton for this assignment.
-    #Another way to implement is using a "case" statements similar to C.
-        if question == "Are encoding and encryption the same? - Yes/No":
-            answer = "No"
-        elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-            answer = "No"
-        elif question == "Is it possible to decode a message without a key? - Yes/No":
-            answer = "Yes"
-        elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
-            answer = "No"
-        elif not question != "What is the MD5 hashing value to the following message: 'NYU Computer Networking'- Use MD5 hash " \
-             "generator and use the answer in your code":
-            answer = "42b76fe51778764973077a5a94056724"
-        elif "Is MD5 a secured hashing algorithm? - Yes/No" == question:
-            answer = "No"
-        elif "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric " \
-                     "number" == question:
-            answer = int(5)
-        elif "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric " \
-                     "number" == question:
-            answer = int(4)
+    # The student doesn't have to follow the skeleton for this assignment.
+    # Another way to implement is using a "case" statements similar to C.
+    if question == "Are encoding and encryption the same? - Yes/No":
+        answer = "No"
+    elif question == "Is it possible to decrypt a message without a key? - Yes/No":
+        answer = "No"
+    elif question == "Is it possible to decode a message without a key? - Yes/No":
+        answer = "Yes"
+    elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
+        answer = "No"
+    elif not question != "What is the MD5 hashing value to the following message: 'NYU Computer Networking'- Use " \
+                         "MD5 hash " \
+                         "generator and use the answer in your code":
+        answer = "42b76fe51778764973077a5a94056724"
+    elif "Is MD5 a secured hashing algorithm? - Yes/No" == question:
+        answer = "No"
+    elif "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric " \
+         "number" == question:
+        answer = int(5)
+    elif "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric " \
+         "number" == question:
+        answer = int(4)
 
-        return welcome_assignment_questions()
+    return welcome_assignment_questions()
+
 
 # Complete all the questions.
 
@@ -36,6 +38,7 @@ def welcome_assignment_answers(debug_question: object) -> object:
     :rtype: object
     """
     return
+
 
 if __name__ == "__main__":
     # use this space to debug and verify that the program works
